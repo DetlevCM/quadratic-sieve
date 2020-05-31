56d3ca270dbda6408d89c270f83593d120ce8709
DetlevCM <detlevcm@googlemail.com>
2020-05-31 12:48:20 +0200

    make it compile error free under Linux - testedm works

---
+++ headers/headers.hpp
@@ -0,0 +1,44 @@
+/*
+ * headers.hpp
+ *
+ *  Created on: 31 May 2020
+ *      Author: detlevcm
+ */
+
+#ifndef HEADERS_HEADERS_HPP_
+#define HEADERS_HEADERS_HPP_
+
+
+#include <iostream>
+#include <fstream>
+//http://msdn.microsoft.com/en-us/library/71t65ya2%28VS.80%29.aspx
+//http://msdn.microsoft.com/en-us/library/6xwbdak2%28v=VS.71%29.aspx
+
+//http://msdn.microsoft.com/en-us/library/aa277852%28v=vs.60%29.aspx
+
+#include <strstream>
+#include <stdio.h>
+#include <mpir.h>
+
+//http://www.math.uni-bayreuth.de/~rbaier/lectures/c++_intro/html/node62.html
+#include <string>
+
+//http://bytes.com/topic/c/answers/621098-window-closes-immediately-after-execution
+//using namespace std;
+
+
+//http://support.microsoft.com/kb/157220
+//http://msdn.microsoft.com/en-us/library/t63833dz%28v=vs.71%29.aspx
+#include <valarray>
+#include <math.h>
+
+// PDF Page 109 - very interesting, checking if perfect square.
+
+// for large arrays -> http://forum.notebookreview.com/programming-homework-questions/553096-regular-vc-arrays-max-size.html#post7133620
+#include <vector> // at the top of your file
+
+// for time: http://www.cplusplus.com/reference/clibrary/ctime/time/
+#include <time.h>
+
+
+#endif /* HEADERS_HEADERS_HPP_ */
--- source/main.cpp
+++ source/main.cpp
@@ -1,40 +1,12 @@
 // DetlevCBoundScProject.cpp : Defines the entry point for the console application.
 //
 
-#include <iostream>
-#include <fstream>
-//http://msdn.microsoft.com/en-us/library/71t65ya2%28VS.80%29.aspx
-//http://msdn.microsoft.com/en-us/library/6xwbdak2%28v=VS.71%29.aspx
 
-//http://msdn.microsoft.com/en-us/library/aa277852%28v=vs.60%29.aspx
-
-#include <strstream.h>
-#include <stdio.h>
-#include <mpir.h>
-
-//http://www.math.uni-bayreuth.de/~rbaier/lectures/c++_intro/html/node62.html
-#include <string>
-
-//http://bytes.com/topic/c/answers/621098-window-closes-immediately-after-execution
-//using namespace std;
-
-
-//http://support.microsoft.com/kb/157220
-//http://msdn.microsoft.com/en-us/library/t63833dz%28v=vs.71%29.aspx
-#include <valarray>
-#include <math.h>
-
-// PDF Page 109 - very interesting, checking if perfect square.
-
-// for large arrays -> http://forum.notebookreview.com/programming-homework-questions/553096-regular-vc-arrays-max-size.html#post7133620
-#include <vector> // at the top of your file
-
-// for time: http://www.cplusplus.com/reference/clibrary/ctime/time/
-#include <time.h>
-
+#include "headers.hpp"
 
 //http://www.cs.hmc.edu/~geoff/classes/hmc.cs070.200401/notes/command-args.html
-char main ( char ,char *argv[]/*Eingabe, int argc, char *argv[]*/)
+//char main ( char ,char *argv[]/*Eingabe, int argc, char *argv[]*/)
+int main(int argc, char* argv[])
 {
 	
 	/* Some general Counters I use */
